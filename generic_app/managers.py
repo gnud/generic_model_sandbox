41b5d21f2798d7ad8555a89cf3a74e7798a4c9b7
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-03-09 10:13:29 +0100

    Added manager methods to create comment article and comment post

--- generic_app/managers.py
+++ generic_app/managers.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 class CommentQSManager(models.query.QuerySet):
-    def create_comment_article(self):
-        pass
+    from generic_app import models as gmodels
 
-    def create_comment_post(self):
-        pass
+    def create_comment_article(self, data: dict):
+        article = self.gmodels.Article.objects.create(**data)
+        self.model.objects.create(content_object=article, comm='df')
+
+    def create_comment_post(self, data: dict):
+        post = self.gmodels.Post.objects.create(**data)
+        self.model.objects.create(content_object=post, comm='df')
 
 
 class CommentNormalQSManager(models.query.QuerySet):
